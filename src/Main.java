9523c4d9e3168f2227aa63df0f1b24be34ea9045
vzemlyanovskaya <viktorry63@yandex.ru>
2021-01-19 01:14:00 +0700

    Remove duplicates

--- src/Main.java
+++ src/Main.java
@@ -7,60 +7,41 @@ public class Main {
   public static void main(String[] args) {
     File gamesDir = new File("C:\\Games");
     StringBuilder log = new StringBuilder("Протокол");
-    File srcDir = new File(gamesDir.getAbsolutePath() + "\\src");
-    if (srcDir.mkdir())
-      log.append("\nСоздан каталог src");
-    File mainDir = new File(srcDir.getAbsolutePath() + "\\main");
-    if (mainDir.mkdir())
-      log.append("\nСоздан каталог main");
-    File mainFile = new File(mainDir, "Main.java");
-    try {
-      if (mainFile.createNewFile())
-        log.append("\nСоздан файл " + mainFile.getName());
-    } catch (IOException e) {
-      log.append(e.toString());
-    }
-    File utilsFile = new File(mainDir, "Utils.java");
-    try {
-      if (utilsFile.createNewFile())
-        log.append("\nСоздан файл " + utilsFile.getName());
-    } catch (IOException e) {
-      log.append(e.toString());
-    }
-    File testDir = new File(srcDir.getAbsolutePath() + "\\test");
-    if (testDir.mkdir())
-      log.append("\nСоздан каталог test");
-    File resDir = new File(gamesDir.getAbsolutePath() + "\\res");
-    if (resDir.mkdir())
-      log.append("\nСоздан каталог res");
-    File drawablesDir = new File(resDir.getAbsolutePath() + "\\drawables");
-    if (drawablesDir.mkdir())
-      log.append("\nСоздан каталог drawables");
-    File vectorsDir = new File(resDir.getAbsolutePath() + "\\vectors");
-    if (vectorsDir.mkdir())
-      log.append("\nСоздан каталог vectors");
-    File iconsDir = new File(resDir.getAbsolutePath() + "\\icons");
-    if (iconsDir.mkdir())
-      log.append("\nСоздан каталог icons");
-    File savegamesDir = new File(gamesDir.getAbsolutePath() + "\\savegames");
-    if (savegamesDir.mkdir())
-      log.append("\nСоздан каталог savegames");
-    File tempDir = new File(gamesDir.getAbsolutePath() + "\\temp");
-    if (tempDir.mkdir()) {
-      log.append("\nСоздан каталог temp");
-    }
-    File tempFile = new File(tempDir, "temp.txt");
-    try {
-      if (tempFile.createNewFile())
-        log.append("\nСоздан файл " + tempFile.getName());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    try (FileWriter writer = new FileWriter(tempFile);) {
+    File srcDir = createFile(gamesDir.getAbsolutePath() + "\\src", log);
+    File mainDir = createFile(srcDir.getAbsolutePath() + "\\main", log);
+    createFile(mainDir, "Main.java", log);
+    createFile(mainDir, "Utils.java", log);
+    createFile(srcDir.getAbsolutePath() + "\\test", log);
+    File resDir = createFile(gamesDir.getAbsolutePath() + "\\res", log);
+    createFile(resDir.getAbsolutePath() + "\\drawables", log);
+    createFile(resDir.getAbsolutePath() + "\\vectors", log);
+    createFile(resDir.getAbsolutePath() + "\\icons", log);
+    createFile(gamesDir.getAbsolutePath() + "\\savegames", log);
+    File tempDir = createFile(gamesDir.getAbsolutePath() + "\\temp", log);
+    File tempFile = createFile(tempDir, "temp.txt", log);
+    try (FileWriter writer = new FileWriter(tempFile)) {
       writer.write(log.toString());
     } catch (IOException e) {
       e.printStackTrace();
     }
     System.out.println("Протокол работы в файле " + tempFile.getName());
+  }
+
+  public static File createFile(String pathname, StringBuilder log) {
+    File dir = new File(pathname);
+    if (dir.mkdir())
+      log.append("\nСоздан каталог " + dir.getName());
+    return dir;
+  }
+
+  public static File createFile(File parent, String child, StringBuilder log) {
+    File file = new File(parent, child);
+    try {
+      if (file.createNewFile())
+        log.append("\nСоздан файл " + file.getName());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return file;
   }
 }
